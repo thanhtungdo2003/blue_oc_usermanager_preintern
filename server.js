cd484955158bf808c43bdd8ab2f3e7eaf46dda3c
BuildTools <unconfigured@null.spigotmc.org>
2025-03-12 11:23:33 +0700

    feat: first commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+    res.send("Server đang chạy trên cổng 3000");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server đang chạy tại http://localhost:${PORT}`);
+});
